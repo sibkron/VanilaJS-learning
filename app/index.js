18195744fd1f751f4dd5879e7e5aaaa0afb417a7
Sergey_Osipov <sibkron@mail.ru>
2022-06-07 17:47:43 +0700

    fundamental js learning

--- app/index.js
+++ app/index.js
@@ -19,9 +19,41 @@ console.log(`${aBinary} & ${bBinary} = ${(a & b).toString(2)}`);
 
 console.log(`${a} | ${b} = ${a | b}`);
 console.log(`${aBinary} | ${bBinary} = ${(a | b).toString(2)}`);
-console.log(`${a} & ${null} = ${a & null}`);
-console.log(`${a} & ${NaN} = ${a & NaN}`);
-console.log(`${a} & ${Infinity} = ${a & Infinity}`);
-console.log(`${a} & ${undefined} = ${a & undefined}`);
+
+console.log(`${a} ^ ${b} = ${a ^ b}`);
+console.log(`${aBinary} ^ ${bBinary} = ${(a ^ b).toString(2)}`);
+
+console.log(`${a} ?? ${NaN} = ${a ?? NaN}`);
+console.log(`${aBinary} ?? ${NaN} = ${(a ?? NaN).toString(2)}`);
+
+console.log(`${a} ?? ${null} = ${a ?? null}`);
+console.log(`${aBinary} ?? ${null} = ${(a ?? null).toString(2)}`);
+
+console.log(`${a} ?? ${undefined} = ${a ?? undefined}`);
+console.log(`${aBinary} ?? ${undefined} = ${(a ?? undefined).toString(2)}`);
+
+console.log(`${a} ?? ${Infinity} = ${a ?? Infinity}`);
+console.log(`${aBinary} ?? ${Infinity} = ${(a ?? Infinity).toString(2)}`);
+
+console.log(`${a} ?? " " = ${a ?? " "}`);
+console.log(`${aBinary} ?? " " = ${(a ?? " ").toString(2)}`);
+
+console.log(`${a} ?? 0 = ${a ?? 0}`);
+console.log(`${aBinary} ?? 0 = ${(a ?? 0).toString(2)}`);
+
+console.log(`${a} ?? 0 = ${a ?? 0}`);
+console.log(`${aBinary} ?? 0 = ${(a ?? 0).toString(2)}`);
+
+console.log(`${~a}  = ${~a}`);
+console.log(`${~aBinary} = ${(~a).toString(2)}`);
+
+console.log(`${a << 2}  = ${a << 2}`);
+console.log(`${aBinary << 2} = ${(a << 2).toString(2)}`);
+
+console.log(`${a >>> 2}  = ${a >>> 2}`);
+console.log(`${aBinary >>> 2} = ${(a >>> 2).toString(2)}`);
+
+console.log(`${a << 2}  = ${a << 2}`);
+console.log(`${aBinary << 2} = ${(a << 2).toString(2)}`);
 
 console.log("--------------------------");
