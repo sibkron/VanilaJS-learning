91ab8160d4a4c509971b37f028e79a32107fe04a
Sergey_Osipov <sibkron@mail.ru>
2022-06-09 19:15:34 +0700

    fudamental js learning

--- app/index.js
+++ app/index.js
@@ -1,34 +1,23 @@
 "use strict";
 
-function fnNew(a, b = "Hello", c = 5) {
-  console.dir({ a, b, c });
-}
+const f1 = (...args) => {
+  console.log(args);
+};
 
-fnNew(1, 2, 3);
-fnNew(1, 2);
-fnNew(1);
-fnNew();
+f1(1, 2, 3);
 
-function fnOld(a, b, c) {
-  b = b || "Hello";
-  c = c || 5;
-  console.dir({ a, b, c });
-}
+const f2 = (...args) => {
+  args.forEach((arg) => {
+    const type = typeof arg;
+    console.log("Type: " + type);
+    if (type === "object") {
+      console.log("Value: " + JSON.stringify(arg));
+    } else {
+      console.log(`Value: ${arg}`);
+    }
+  });
+};
 
-fnOld(1, 2, 3);
-fnOld(1, 2);
-fnOld(1);
-fnOld();
-
-function fnHash(args) {
-  args.a = args.a || [7, 25, 10];
-  args.b = args.c || "Hello";
-  args.c = args.c || 5;
-  console.dir(args);
-}
-
-fnHash({ a: [1, 2, 3], b: "Hi", c: 3 });
-fnHash({ b: "World" });
-fnHash({ c: 7 });
+f2(1, "Marcus", { field: "value" });
 
 console.log("--------------------------");
