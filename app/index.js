0e10e280871f6c5a059b1bf35e2ca6ce7269673d
Sergey_Osipov <sibkron@mail.ru>
2022-06-15 17:15:21 +0700

    fundamental vanilla js: serlization

--- app/index.js
+++ app/index.js
@@ -6,43 +6,14 @@ const person = {
   born: 121,
 };
 
-if ("name" in person) {
-  console.log(`Person name is ${person.name}`);
-}
+const s = JSON.stringify(person);
+console.log(s);
 
-for (const key in person) {
-  const value = person[key];
-  console.dir({ key, value });
-}
+const obj = JSON.parse(s);
+console.dir(obj);
 
-const name = "Marcus Aurelius";
-const city = "Roma";
-
-const person1 = { name: name, city: city };
-const person2 = { name, city };
-
-const fieldName = "city";
-const fieldValue = "Roma";
-
-const person3 = {
-  name: "Marcus Aurelius",
-  [fieldName]: fieldValue,
-};
-
-const person4 = {
-  name: "Marcus Aurelius",
-  ["city" + "Born"]: fieldValue,
-};
-
-function fn(s) {
-  return s + "Born";
-}
-
-const person5 = {
-  name: "Marcus Aurelius",
-  [fn("city")]: fieldValue,
-};
-
-console.dir({ person1, person2, person3, person4, person5 });
+const letters = ["A", "B", "C", "D"];
+console.log(letters + "");
+console.log(JSON.stringify(letters));
 
 console.log("--------------------------");
