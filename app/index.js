c65d19e3a48128b54b2203e606b12f016f1daa20
Sergey_Osipov <sibkron@mail.ru>
2022-06-17 15:09:00 +0700

    js undamental learning

--- app/index.js
+++ app/index.js
@@ -1,40 +1,37 @@
 "use strict";
 
-{
-  const matrix = [
-    [7, 10, 1, 5, 2],
-    [6, -1, 7, 2, 3],
-    [1, 2, 4, -8, 2],
-    [-6, 4, 8, 2, 0],
-  ];
-
-  const max = (a, b) => (a > b ? a : b);
-
-  const res = matrix
-    .map((row) => row.reduce(max))
-    .reduce((acc, rowMax) => acc + rowMax);
-
-  console.log(res);
-
-  for (const i in matrix) {
-    const row = matrix[i];
-    for (const j in row) {
-      const col = row[j];
-      console.log(i, j, col);
-    }
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 
-  matrix.forEach((row, i) => {
-    row.forEach((col, j) => {
-      console.log(i, j, col);
-    });
-  });
+  static from(obj) {
+    const { x, y } = obj;
+    return new Point(x, y);
+  }
+
+  move(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  toString() {
+    return `[${this.x}, ${this.y}]`;
+  }
 }
 
-let count = 0;
-const arr = [7, 10, 1, 5, 2];
-const sum = (acc, val) => (count++, acc + val);
-const res = arr.reduce(sum);
-console.log({ res, count });
+console.log("function prototype: ", function () {}.prototype);
+console.log("lambda prototype: ", (() => {}).prototype);
+
+console.log("Point prototype: ", Point.prototype);
+console.log("move prototype: ", Point.prototype.move.prototype);
+
+const p1 = new Point(10, 20);
+p1.move(-5, 10);
+
+console.log(p1);
+console.log(p1.toString());
+console.log(p1 + "");
 
 console.log("--------------------------");
