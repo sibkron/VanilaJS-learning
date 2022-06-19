42fd257d9f146e2ce8361e1f88666eb4f3508671
Sergey_Osipov <sibkron@mail.ru>
2022-06-19 11:36:54 +0700

    python fundamental learning

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 list = [int(x) for x in input("Введите числа через пробел:").split()]
 print(list)
+
+
+def f(ham: str, eggs: str = 'eggs') -> str:
+    print("Annotations:", f.__annotations__)
+    print("Arguments:", ham, eggs)
+    return ham + ' and ' + eggs
+
+
+print(f('spam'))
