39f26da574fed5355ab5cbf0134c1ee9c3e4ec66
Sergey_Osipov <sibkron@mail.ru>
2022-06-20 20:15:13 +0700

    js's functions

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 "use strict";
 
-const log = (base, n) => Math.log(n) / Math.log(base);
+const partial =
+  (fn, x) =>
+  (...args) =>
+    fn(x, ...args);
 
-const createLog = (base) => (n) => log(base, n);
+const sum4 = (a, b, c, d) => a + b + c + d;
 
-const lg = createLog(10);
-const ln = createLog(Math.E);
+const f11 = partial(sum4, 1);
+const f12 = partial(f11, 2);
+const f13 = partial(f12, 3);
+const y1 = f13(4);
+console.log(y1);
 
-console.log(`lg(5) = ${lg(5)}`);
-console.log(`ln(5) = ${ln(5)}`);
+const f21 = partial(sum4, 1, 2);
+const f22 = partial(f21, 3);
+const y2 = f22(4);
+console.log(y2);
 
 console.log("--------------------------");
