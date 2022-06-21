7575ec09b45dae071a1d35545bcfac725555190b
Sergey_Osipov <sibkron@mail.ru>
2022-06-21 09:32:19 +0700

    python's fundamental

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from re import X
+num = input("Введите число: ")
+if int(num) > 0:
+    if int(num) > 10:
+        print("Вы ввели число больше 10")
+        if int(num) > 50:
+            print("Вы ввели число больше 50")
+    else:
+        print("Вы ввели число меньше 10 и больше 0")
+elif int(num) < -10:
+    print("Вы ввели число меньше -10!")
+else:
+    print("Вы ввели число меньше 0 и больше -10!")
+print("All is okay!")
 
-
-list = [int(x) for x in input("Введите числа через пробел:").split()]
-print(list)
-
-
-def f(ham: str, eggs: str = 'eggs') -> str:
-    print("Annotations:", f.__annotations__)
-    print("Arguments:", ham, eggs)
-    return ham + ' and ' + eggs
-
-
-print(f('spam'))
-
-x, y = 10, 20
-print(x, y)
-x, y = y, x
-print(x, y)
+name = input()
+A = 'Yes' if name != "Test" else 'No'
+print(A)
