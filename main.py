5c03cb775fcaceb73e8d5312fdd41fc6b96c8014
Sergey_Osipov <sibkron@mail.ru>
2022-06-21 11:06:07 +0700

    python fundamental

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-num = input("Введите число: ")
-if int(num) > 0:
-    if int(num) > 10:
-        print("Вы ввели число больше 10")
-        if int(num) > 50:
-            print("Вы ввели число больше 50")
-    else:
-        print("Вы ввели число меньше 10 и больше 0")
-elif int(num) < -10:
-    print("Вы ввели число меньше -10!")
-else:
-    print("Вы ввели число меньше 0 и больше -10!")
-print("All is okay!")
+i = 1000
+while i > 100:
+    print(i)
+    i /= 2
 
-name = input()
-A = 'Yes' if name != "Test" else 'No'
-print(A)
+for j in 'Hello world':
+    if j == 'w':
+        break
+    print(j * 2, end='')
+else:
+    print("\nБуквы а нет в слове")
