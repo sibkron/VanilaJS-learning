ff02ec6969229f2e94394f733bcb65c4581af0f6
Sergey_Osipov <sibkron@mail.ru>
2022-06-21 14:45:58 +0700

    python fundamental

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-i = 1000
-while i > 100:
-    print(i)
-    i /= 2
+l = []
+lis = [1, 56, 'x', 34, 2.34, ['S', 't', 'r', 'o', 'k', 'a']]
+print(lis)
+a = [a + b for a in 'list' if a != 's' for b in 'soup' if b != 'u']
+print(a)
 
-for j in 'Hello world':
-    if j == 'w':
-        break
-    print(j * 2, end='')
-else:
-    print("\nБуквы а нет в слове")
+l.append(23)
+l.append(34)
+print(l)
+b = [24, 67]
+l.extend(b)
+print(l)
+l.insert(1, 56)
+print(l)
+l.append(34)
+l.remove(34)
+print(l)
+l.pop(0)
+print(l)
+print(l.index(56))
+print(l.count(34))
+l.sort()
+print(l)
+l.reverse()
+print(l)
+l.clear()
+print(l)
