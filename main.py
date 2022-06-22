51b8eb0a949c8fe19c8b2a15e04e6dabc5d148a3
Sergey_Osipov <sibkron@mail.ru>
2022-06-22 16:52:14 +0700

    python fundamental learning

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-l = []
-lis = [1, 56, 'x', 34, 2.34, ['S', 't', 'r', 'o', 'k', 'a']]
-print(lis)
-a = [a + b for a in 'list' if a != 's' for b in 'soup' if b != 'u']
-print(a)
+l = [34, 'sd', 56, 34.34]
 
-l.append(23)
-l.append(34)
-print(l)
-b = [24, 67]
-l.extend(b)
-print(l)
-l.insert(1, 56)
-print(l)
-l.append(34)
-l.remove(34)
-print(l)
-l.pop(0)
-print(l)
-print(l.index(56))
-print(l.count(34))
-l.sort()
-print(l)
-l.reverse()
-print(l)
-l.clear()
-print(l)
+i = 0
+while i < 4:
+    print(l[i])
+    i += 1
+
+# items[START:STOP:STEP]
+print(l[:])
+print(l[1:])
+print(l[:3])
+print(l[::2])
+print(l[::-2])
+
+a = (43, 56, 42.23, 'sd')
+b = [43, 56, 42.23, 'sd']
+print(a.__sizeof__())
+print(b.__sizeof__())
