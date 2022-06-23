7c3c40b077acfe0e880102d5547ade0fba10e020
Sergey_Osipov <sibkron@mail.ru>
2022-06-23 13:36:46 +0700

    python closures

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-d = {a: a**2 for a in range(7)}
-print(d)
+def func(x):
+    def add(a):
+        return x + a
+    return add
 
-person = {'name': {'last_name': 'Иванов',
-                   'first_name': 'Иван', 'middle_name': 'Иванович'}, 'address': ['г. Андрюшки', 'ул. Васильевская д.236']}
-print(person['name']['first_name'])
-print(person.values())
+
+test = func(100)
+print(test(200))
