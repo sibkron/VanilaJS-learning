e9111871aed780507329819c7655d06e9f96e2b3
Sergey_Osipov <sibkron@mail.ru>
2022-06-24 09:56:28 +0700

    js fundamental: callbacks

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-const logable =
-  (fn) =>
-  (...args) => {
-    const res = fn(...args);
-    console.log(`Call: ${fn.name}(${args.join(",")}) Result: ${res}`);
-    return res;
-  };
+const fs = require("fs");
 
-//Usage
-const sum = (a, b) => a + b;
+const add = (a, b) => a + b;
+const sum = (a, b, callback) => callback(a + b);
 
-const wrapped = logable(sum);
-wrapped(3, 5);
+console.log("Use add: " + add(5, 2));
+sum(5, 2, console.log.bind(null, "Use sum:"));
 
 console.log("--------------------------");
+
+fs.readFile("./a.txt", "utf-8", (err, data) => {
+  console.log({ lines: data.toString().split("\n").length });
+});
+
+console.log("end");
