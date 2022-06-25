1da2e74837b7f197e2f0f6ae7365acd620654127
Sergey_Osipov <sibkron@mail.ru>
2022-06-25 13:34:05 +0700

    js fundamental: callbacks-2

--- app/index.js
+++ app/index.js
@@ -2,16 +2,12 @@
 
 const fs = require("fs");
 
-const add = (a, b) => a + b;
-const sum = (a, b, callback) => callback(a + b);
+const print = (err, data) => {
+  console.log({ lines: data.toString().split("\n").length });
+};
 
-console.log("Use add: " + add(5, 2));
-sum(5, 2, console.log.bind(null, "Use sum:"));
+const fileName = "./a.txt";
+
+fs.readFile(fileName, "utf-8", print);
 
 console.log("--------------------------");
-
-fs.readFile("./a.txt", "utf-8", (err, data) => {
-  console.log({ lines: data.toString().split("\n").length });
-});
-
-console.log("end");
--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-try:
-    x = int(input())
-except ValueError:
-    print("X должен быть числом!")
-    x = 0
+txtFile = open('./app/a.txt')
 
-try:
-    y = int(input())
-except ValueError:
-    print("Y должен быть числом!")
-    y = 0
-else:
-    print("Все верно!")
-finally:
-    print("Вывести в любом случае!")
+for line in txtFile:
+    print(line)
 
-try:
-    res = x / y
-    print(res)
-except ZeroDivisionError:
-    print("Ошибка! На 0 делить нельзя!")
+txtFile.close()
