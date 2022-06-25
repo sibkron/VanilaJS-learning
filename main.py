b024d6e59662af7e6b0ed5086ec53f685c5de375
Sergey_Osipov <sibkron@mail.ru>
2022-06-25 22:53:20 +0700

    OOP python

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-txtFile = open('./app/a.txt')
+class Person:
+    name = "Ivan"
+    age = 40
 
-for line in txtFile:
-    print(line)
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-txtFile.close()
+    def set(self, name, age):
+        self.name = name
+        self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
+
+vlad = Person("Vlad", 38)
+print(vlad.getName())
+
+petr = Person("Petr", 35)
+print(petr.name)
