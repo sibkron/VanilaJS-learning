513c9f69ef5b9fc03ba7b59793b3dd1e0a587e57
Sergey_Osipov <sibkron@mail.ru>
2022-06-27 09:01:55 +0700

    python fundamental: oop && decorators

--- main.py
+++ main.py
@@ -17,8 +17,52 @@ class Person:
         return self.age
 
 
+class Student (Person):
+    course = 1
+
+    def __init__(self, name, age, course):
+        super().__init__(name, age)
+        self.course = course
+
+    def set(self, name, age, course):
+        super().set(name, age)
+        self.course = course
+
+
 vlad = Person("Vlad", 38)
 print(vlad.getName())
 
 petr = Person("Petr", 35)
 print(petr.name)
+
+igor = Student("Igor", 22, 2)
+print(igor.name + ", student of " + str(igor.course) + " course")
+igor.set("Igor", 22, 3)
+print(igor.name + ", student of " + str(igor.course) + " course")
+
+# decorators
+
+
+def decorator(func):
+    def wrapper():
+        print("Код до выполнения функции")
+        func()
+        print("Код после выполнения функции")
+    return wrapper
+
+
+def test(func):
+    def wrapper():
+        print("Код до выполнения функции 2")
+        func()
+        print("Код после выполнения функции 2")
+    return wrapper
+
+
+@decorator
+@test
+def show():
+    print("Я обычная функция")
+
+
+show()
