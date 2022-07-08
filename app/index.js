6485f09c557cd84b5ba46c6bc6bf87b511a0f101
Sergey_Osipov <sibkron@mail.ru>
2022-07-08 17:31:25 +0700

    js fundamental: regexp 2

--- app/index.js
+++ app/index.js
@@ -1,40 +1,29 @@
 "use strict";
 
-const rx1 = /abc/;
-console.log("Do you know abc?".match(rx1));
+const rx = /[abc]/gi;
+console.dir({
+  rx,
+  flags: rx.flags,
+  global: rx.global,
+  ignoreCase: rx.ignoreCase,
+  multiline: rx.multiline,
+  source: rx.source,
+  sticky: rx.sticky,
+  unicode: rx.unicode,
+  lastIndex: rx.lastIndex,
+});
 
-const rx2 = new RegExp("abc");
-console.log("Do you know abc?".match(rx2));
+console.dir({
+  xyz: rx.test("xyz"),
+  abc: rx.test("abc"),
+});
 
-const rx3 = /[a-z]+a[a-z]+/g;
-const st3 = "A man can die but once";
-console.log(st3.match(rx3));
+const s = "abcdefgabc";
 
-const rx4 = /\sg\w*/g;
-const st4 =
-  "Some are born great, " +
-  "some achieve greatness, " +
-  "and some have greatness thrust upon them.";
-console.log(st4.match(rx4));
-
-const rx5 = /.u../g;
-const st5 = "- Such much? - For whow how";
-console.log(st5.match(rx5));
-
-const rx6 = /\w{3,5}/g;
-const st6 = "- MGIMO finished? - Ask?!";
-console.log(st6.match(rx6));
-
-const rx7 = /[^l] /g;
-const st7 = "Nothing will come of nothing";
-console.log(st7.match(rx7));
-
-const rx8 = /^\+?\d{12}$/;
-const st8 = "+380661234567";
-console.log(st8.match(rx8));
-
-const rx9 = /[0-9]+ (hours|days)/g;
-const st9 = "5 days";
-console.log(st9.match(rx9));
+let res;
+do {
+  res = rx.exec(s);
+  console.log({ lastIndex: rx.lastIndex, res });
+} while (res);
 
 console.log("--------------------------");
