8a422cf6d98b433c0b80997e215a7fed7fb78497
Sergey_Osipov <sibkron@mail.ru>
2022-07-08 09:13:22 +0700

    js fundamental: regexp

--- app/index.js
+++ app/index.js
@@ -10,4 +10,11 @@ const rx3 = /[a-z]+a[a-z]+/g;
 const st3 = "A man can die but once";
 console.log(st3.match(rx3));
 
+const rx4 = /\sg\w*/g;
+const st4 =
+  "Some are born great, " +
+  "some achieve greatness, " +
+  "and some have greatness thrust upon them.";
+console.log(st4.match(rx4));
+
 console.log("--------------------------");
