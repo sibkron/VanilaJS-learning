807d20bb07686ae979e73b12f125c0f9ce626f23
Sergey_Osipov <sibkron@mail.ru>
2022-07-10 20:55:15 +0700

    js fundamental: regexp 3

--- app/index.js
+++ app/index.js
@@ -1,21 +1,32 @@
 "use strict";
 
-const s = "Hello <user1@domain.> and user2@domain.com";
+const rx1 = /abc/g;
+const s11 = "abcdef";
+const s12 = s11.replace(rx1, "123");
+console.log(s12);
 
-const rx = /[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+/g;
+const rx2 = new RegExp("abc", "gi");
+const s21 = "abcdefgABCDEFG";
+const s22 = s21.replace(rx2, "789");
+console.log(s22);
 
-const m = s.match(rx);
+const rx3 = /abc/g;
+const s = "abcdefabc";
+const res = s.replace(rx3, (sub, pos, str) => {
+  console.dir({ sub, pos, str });
+  return sub.toUpperCase();
+});
 
-console.dir(m);
+console.log(res);
 
-const words = (s) => {
-  let mySet = new Set(s.toLowerCase().replace(/\W+/g, " ").trim().split(/\s/));
-  return [...mySet];
-};
+const rx4 = /def/g;
+const s1 = "abcdefgabc";
+const res1 = s1.search(rx4);
+console.log(res1);
 
-const s1 = "Hello World, here we are!";
-
-console.dir(words(s1));
-console.dir(words(s1 + s1));
+const rx5 = /cba/g;
+const s2 = "abcdefgabc";
+const res2 = s2.search(rx5);
+console.log(res2);
 
 console.log("--------------------------");
