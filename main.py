78baab558925e9c9eb4b392e18c2f6daeac63ab4
Sergey_Osipov <sibkron@mail.ru>
2022-07-15 20:56:08 +0700

    python fundamental

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 def main():
-    class MyClass:
-        def __init__(self, name="Белый"):
-            self.name = name
-            print("Создан объект: ", self.name)
-
-        def __del__(self):
-            print("Удален объект: ", self.name)
-
-    def create(name):
-        obj = MyClass(name)
-
-    A = MyClass()
-    B = MyClass("Красный")
-    C = MyClass("Синий")
-
-    create("Желтый")
-
-    C.name = "Желтый"
-
-    del A
-    del B
-    del C
+    A = dict(zip([1, 2, 3], ['K', 'L', 'M']))
+    B = dict.fromkeys([10, 20, 30], 'Z')
+    print("A=", A)
+    print("B=", B)
+    print("Сравнение словарей:")
+    print("A==B:", A == B)
+    print("A!=B:", A != B)
+    A.update(B)
+    print("Объединение словарей:")
+    print("A=", A)
+    print("Проверка содержимого словарей:")
+    print((20, 'Z') in A.items())
+    print(20 in A)
+    print('Z' in A)
+    A.clear()
+    print("Словарь после очистки:")
+    print("A=", A)
 
 
 if __name__ == "__main__":
