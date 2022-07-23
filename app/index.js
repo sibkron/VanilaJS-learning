a143f92d6b9643d7e5241e9c347a672a3694167f
Sergey_Osipov <sibkron@mail.ru>
2022-07-23 12:53:13 +0700

    js fundamental: console 2

--- app/index.js
+++ app/index.js
@@ -1,49 +1,39 @@
 "use strict";
 
-console.clear();
+const readline = require("readline");
 
-console.log("log");
-
-const obj = {
-  name: "Marcus Aurelius",
-  city: "Roma",
-  born: 121,
-  children: [
-    {
-      name: "Vibia Aurelia Sabina",
-      city: "Sirmium",
-      born: 170,
-    },
-    {
-      name: "Annia Cornificia Faustina Minor",
-      city: "Roma",
-      born: 160,
-    },
-  ],
-};
-
-Object.defineProperty(obj, "childCount", {
-  enumerable: false,
-  writable: false,
-  value: 13,
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
 });
 
-console.log({ obj });
+rl.prompt();
 
-console.dir({ obj });
-console.dir({ obj }, { showHidden: true, depth: 20, colors: true });
+const commands = {
+  help() {
+    console.log("Commands: ", Object.keys(commands).join(", "));
+  },
+  hello() {
+    console.log("Hello there!");
+  },
+  exit() {
+    rl.close();
+  },
+};
 
-console.error("Error");
-
-console.time("Loop time");
-const arr = [];
-for (let i = 0; i < 10000; i++) {
-  arr.push(i);
-}
-console.timeEnd("Loop time");
-
-console.trace("Trace here");
-
-console.log("\n" + Object.keys(console).join(", "));
+rl.on("line", (line) => {
+  line = line.trim();
+  const command = commands[line];
+  if (command) {
+    command();
+  } else {
+    console.log("Uknown command");
+  }
+  rl.prompt();
+}).on("close", () => {
+  console.log("Bye!");
+  process.exit(0);
+});
 
 console.log("--------------------------");
