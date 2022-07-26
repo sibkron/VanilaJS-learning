ae0e37841798075a0274b9849675bb960ff19fb2
Sergey_Osipov <sibkron@mail.ru>
2022-07-26 13:22:20 +0700

    js fundamental: chaining 4

--- app/index.js
+++ app/index.js
@@ -1,41 +1,21 @@
 "use strict";
 
-class Text {
-  constructor(s) {
-    this.value = s;
-  }
-
-  line(a) {
-    this.value += "\n" + a;
-    return this;
-  }
-
-  toString() {
-    return this.value;
-  }
-}
-
-const txt = new Text("line1").line("line2").line("line3").line("line4");
-
-console.log(`${txt}`);
-
-const text1 = (s = "") => ({
-  line: (a) => text1(`${s}\n${a}`),
-  toString: () => s,
-});
-
-const txt1 = text1("line1").line("line2").line("line3").line("line4");
-console.log(`${txt1}`);
-
-const text2 = (
+const text = (
   s = "",
   o = {
     line: (a) => ((s += "\n" + a), o),
-    toString: () => s,
+    [Symbol.iterator]: () => ({
+      next() {
+        const res = { value: s, done: this.finished };
+        this.finished = true;
+        return res;
+      },
+    }),
   }
 ) => o;
 
-const txt2 = text2("line1").line("line2").line("line3").line("line4");
-console.log(`${txt2}`);
+const txt = text("line1").line("line2").line("line3").line("line4");
+
+console.log(...txt);
 
 console.log("--------------------------");
