b0469b192d6a7ddecf5e8c389c2654cf7f63382c
Sergey_Osipov <sibkron@mail.ru>
2022-08-01 14:50:53 +0700

    js fundamental: typescript

--- first.js
+++ first.js
@@ -1,3 +1,14 @@
+var Weekday1;
+(function (Weekday1) {
+    Weekday1[Weekday1["MON"] = 0] = "MON";
+    Weekday1[Weekday1["TUE"] = 1] = "TUE";
+    Weekday1[Weekday1["WED"] = 2] = "WED";
+    Weekday1[Weekday1["THU"] = 3] = "THU";
+    Weekday1[Weekday1["FRI"] = 4] = "FRI";
+    Weekday1[Weekday1["SAT"] = 5] = "SAT";
+    Weekday1[Weekday1["SUN"] = 6] = "SUN";
+})(Weekday1 || (Weekday1 = {}));
+;
 const replace = (arr, target, replacement) => {
     for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(target) && target.indexOf(arr[i])
@@ -6,9 +17,19 @@ const replace = (arr, target, replacement) => {
         }
     }
 };
+let w = 'Mon';
+const distanceFromOrigin = (p) => Math.pow(p.x, 2) + Math.pow(p.y, 2);
+let code = [404, 'not found'];
+const root = (x) => {
+    if (x >= 0)
+        return Math.sqrt(x);
+    else
+        return [404, 'not found'];
+};
 const a = [11, 12, 13, 14, 15, 16];
 console.dir({ a });
 replace(a, 13, 0);
 console.dir({ a });
 replace(a, [13, 14], 0);
 console.dir({ a });
+console.log(w);
--- first.ts
+++ first.ts
@@ -1,4 +1,10 @@
-const replace = (arr: number[], target: number | number[], replacement: number) => {
+type Numbers = number | number[];
+type Weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | 'Sun';
+enum Weekday1 { MON, TUE, WED, THU, FRI, SAT, SUN };
+type Point = { x: number, y: number };
+type ErrorCode = [number, string];
+
+const replace = (arr: number[], target: Numbers, replacement: number) => {
     for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(target) && target.indexOf(arr[i])
             || !Array.isArray(target) && target === arr[i]) {
@@ -7,9 +13,21 @@ const replace = (arr: number[], target: number | number[], replacement: number) 
     }
 }
 
+let w: Weekday = 'Mon';
+const distanceFromOrigin = (p: Point) => Math.pow(p.x, 2) + Math.pow(p.y, 2);
+let code: ErrorCode = [404, 'not found'];
+
+const root = (x: number): number | ErrorCode => {
+    if (x >= 0)
+        return Math.sqrt(x);
+    else
+        return [404, 'not found'];
+}
+
 const a = [11, 12, 13, 14, 15, 16];
 console.dir({a});
 replace(a, 13, 0);
 console.dir({a});
 replace(a, [13, 14], 0);
-console.dir({a});
+console.dir({ a });
+console.log(w);
