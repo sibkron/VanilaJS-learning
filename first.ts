df59e108ffd926ee30c6fda5a4e971e66bc73c96
Sergey_Osipov <sibkron@mail.ru>
2022-08-02 21:20:14 +0700

    js fundamenatal: typescript 3

--- first.ts
+++ first.ts
@@ -1,91 +1,38 @@
-type Numbers = number | number[];
-type Weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | 'Sun';
-enum Weekday1 { MON, TUE, WED, THU, FRI, SAT, SUN };
-type ErrorCode = [number, string];
-type MaybeColorPoint = {
-    x: number,
-    y: number,
-    color?: string
-};
-
-interface Identifiable {
-    id(): string
+type ExtractArray<T> = T extends any[] ? T : never;
+type Data = string | string[] | number | number[];
+type ArrayData = ExtractArray<Data>;
+type Point = {
+    [propname in 'x'|'y']: number
+}
+type ReadonlyEmployee = {
+    readonly [propname in keyof Employee]: Employee[propname]
 }
 
-type Employable = Identifiable & {
-    salary(): number
+type AllRequired<T> = {
+    [propname in keyof T]-?: T[propname]
 }
-
-type Dictionary = {
-    creator: string,
-    [arg: string]: string | string[]
-}
-
-const dict: Dictionary = { creator: 'Pierre' };
-dict.hello = ['bonjour', 'salut', 'allô'];
-let str = 'world';
-dict[str] = 'monde';
-
-const average = (x: number, y?: number) => y === undefined ? x : (x + y) / 2;
-
-const replace = (arr: number[], target: Numbers, replacement: number) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(target) && target.indexOf(arr[i])
-            || !Array.isArray(target) && target === arr[i]) {
-            arr[i] = replacement;
-        }
+class Entry<K, V> {
+    key: K;
+    value: V;
+    constructor(key: K, value: V) {
+        this.key = key;
+        this.value = value;
     }
 }
 
-let w: Weekday = 'Mon';
-let code: ErrorCode = [404, 'not found'];
+type Pair<T> = { first: T, second: T };
 
-const root = (x: number): number | ErrorCode => {
-    if (x >= 0)
-        return Math.sqrt(x);
-    else
-        return [404, 'not found'];
+function count<T>(arr: T[], target: T){
+    let count = 0;
+    for (let e of arr) if (e === target) count++;
+    return count;
 }
 
-class Point {
-    #x: number;
-    #y: number;
+let digits = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+let result1 = count(digits, 5);
 
-    constructor(x: number, y: number) {
-        this.#x = x;
-        this.#y = y;
-    }
+const tail = <T extends { slice(from: number, to?: number): T }>(values: T) =>
+    values.slice(1);
 
-    distance(other: Point) {
-        return Math.sqrt(Math.pow(this.#x - other.#x, 2) + Math.pow(this.#y - other.#y, 2))
-    }
-    
-    toString() { return `(${this.#x}, ${this.#y})` }
-
-    static origin = new Point(0, 0);
-}
-
-const mkString = (values: unknown[], { separator, leftDelimiter, rightDelimiter }:
-    { separator: string, leftDelimiter: string, rightDelimiter: string }) =>
-    leftDelimiter + values.join(separator) + rightDelimiter;
-
-class Remover {
-    c: string
-    constructor(c: string) { this.c = c }
-    removeFrom(str: string): string
-    removeFrom(str: string[]): string[]
-    removeFrom(str: string | string[]) {
-    if (Array.isArray(str))
-        return str.map(s => s.replace(this.c, ''))
-    else
-        return str.replace(this.c, '')
-    }
-}    
-
-const a = [11, 12, 13, 14, 15, 16];
-console.dir({a});
-replace(a, 13, 0);
-console.dir({a});
-replace(a, [13, 14], 0);
-console.dir({ a });
-console.log(w);
+let result = tail([1, 7, 2, 9]);
+console.dir({ result });    
