d54a7d31c9e70f35dbb81d9f7dd07d57c18189b4
Sergey_Osipov <sibkron@mail.ru>
2022-08-03 19:32:57 +0700

    js fundamental: typescript 2

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function getFinalPrice(price, discount) {
+    return price - price / discount;
+}
+console.log(getFinalPrice(100, 10));
+console.log(getFinalPrice(100, 8));
+//# sourceMappingURL=main.js.map
