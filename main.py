b381e2b800cebbf61e28b692c20bf74ccf8cd921
Sergey_Osipov <sibkron@mail.ru>
2022-08-05 20:38:30 +0700

    python fundamental

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 def main():
-    digit = str(input("Введите число: "))
+    a = input("Введите коэффициент уравнения A:")
+    b = input("Введите коэффициент уравнения B:")
 
-    def digitReplace(num):
-        return ''.join(list(map(lambda x: str(9 - int(x)), num)))
-
-    print(digitReplace(digit))
-
-    firstList = [12, 3, 456, 78]
-
-    def digitJoin(num):
-        return ''.join(list(map(lambda x: str(x), num)))
-
-    print(digitJoin(firstList))
-
-    def digitSrav(list1, list2):
-        return str(True if list1.sort() == list2.sort() else False)
-
-    print(digitSrav([1, 2, 3, 4, 5, 6], [2, 4, 5, 1, 3, 6]))
+    try:
+        intA = int(a)
+        intB = int(b)
+        print([intA, intB])
+        if intA == 0 and intB == 1:
+            print("Решение уравнения любое")
+        elif intA == 0 and intB != 1:
+            print("Уравнение не имеет корней")
+        else:
+            res = (intB - 1) / (intA - 1)
+            print(
+                f"Решение уравнения с коэффициаентами {intA} и {intB} равно {res}")
+    except Exception:
+        print("Введенные данные нельзя использовать в качестве коэффициентов!")
 
 
 if __name__ == "__main__":
