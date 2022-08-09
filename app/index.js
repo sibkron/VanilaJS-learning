4d08787ba0196f75013addb022384294b9340902
Sergey_Osipov <sibkron@mail.ru>
2022-08-09 17:15:14 +0700

    js fundamental: iteratirs

--- app/index.js
+++ app/index.js
@@ -1,53 +1,37 @@
 "use strict";
 
-console.log("Example 1");
-
-for (let i = 0; i < 3; i++) {
-  console.log(i);
-}
-
-console.log("Example 2");
-
-{
-  let i = 0;
-  for (; i < 3; i++) {
-    console.log(i);
-  }
-}
-
-console.log("Example 3");
-
-{
-  for (let i = 0; i < 3; ) {
-    console.log(i++);
-  }
-}
-
-let a = 0;
-while (a < 10) {
-  console.log(a++);
-}
-
-let j = 0;
-do {
-  console.log(j++);
-} while (j < 10);
-
-const hash = {
-  first: 7,
-  second: 10,
-  third: 1,
-  fourth: 5,
+const range = {
+  start: 1,
+  end: 1000,
+  [Symbol.asyncIterator]() {
+    let value = this.start;
+    return {
+      next: () =>
+        Promise.resolve({
+          value,
+          done: value++ === this.end + 1,
+        }),
+    };
+  },
 };
 
-hash.fifth = 2;
+console.dir({
+  range,
+  names: Object.getOwnPropertyNames(range),
+  symbols: Object.getOwnPropertySymbols(range),
+});
 
-for (const key in hash) {
-  const value = hash[key];
-  console.table({
-    key: typeof key,
-    value: typeof value,
-  });
-}
+let k = 0;
+
+const timer = setInterval(() => {
+  console.log("next ", k++);
+}, 10);
+
+(async () => {
+  for await (const number of range) {
+    console.log(number);
+  }
+  clearInterval(timer);
+})();
 
 console.log("--------------------------");
