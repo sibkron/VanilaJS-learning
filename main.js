b61bb07e51afa2d84bdbb474723de7b0782dd1df
Sergey_Osipov <sibkron@mail.ru>
2022-08-16 10:31:27 +0700

    tsc fundamental

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 "use strict";
-function padLeft(value, padding) {
-    if (typeof padding === "number") {
-        return Array(padding + 1).join(" ") + value;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SearchFailedAction = exports.SearchSuccessAction = exports.SearchAction = void 0;
+class SearchAction {
+    constructor(payload) {
+        this.payload = payload;
+        this.actionType = "SEARCH";
     }
-    if (typeof padding === "string") {
-        return padding + value;
-    }
-    throw new Error(`Expected string or number, got '${padding}'.`);
 }
-console.log(padLeft("Hello world", 4));
-console.log(padLeft("Hello world", "John says "));
-console.log(padLeft("Hello world", true));
+exports.SearchAction = SearchAction;
+class SearchSuccessAction {
+    constructor(payload) {
+        this.payload = payload;
+        this.actionType = "SEARCH_SUCCESS";
+    }
+}
+exports.SearchSuccessAction = SearchSuccessAction;
+class SearchFailedAction {
+    constructor() {
+        this.actionType = "SEARCH_FAILED";
+    }
+}
+exports.SearchFailedAction = SearchFailedAction;
+function area(shape) {
+    switch (shape.kind) {
+        case "rectangle": return shape.heigth * shape.width;
+        case "circle": return Math.PI * shape.radius ** 2;
+    }
+}
+const myRectangle = { kind: "rectangle", width: 10, heigth: 20 };
+console.log(`Rectangle's area is ${area(myRectangle)}`);
+const myCircle = { kind: "circle", radius: 10 };
+console.log(`Circle's area is ${area(myCircle)}`);
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,13 +1,43 @@
-type Foot = number;
-type Pound = number;
+export class SearchAction{
+    actionType = "SEARCH";
 
-type Patient = {
-    name: string:
-    height: Foot;
-    weight?: Pound;
+    constructor(readonly payload: {searchQuery: string}) {}
 }
 
-let patient: Patient = {
-    name: 'Joe Smith',
-    height: 5
+export class SearchSuccessAction{
+    actionType = "SEARCH_SUCCESS";
+
+    constructor(public payload: {searchResults: string[]}){}
 }
+
+export class SearchFailedAction{
+    actionType = "SEARCH_FAILED";
+}
+
+export type SearchActions = SearchAction | SearchSuccessAction | SearchFailedAction;
+
+interface Rectangle{
+    kind: "rectangle";
+    width: number;
+    heigth: number;
+}
+
+interface Circle{
+    kind: "circle";
+    radius: number;
+}
+
+type Shape = Rectangle | Circle;
+
+function area(shape: Shape) {
+    switch (shape.kind) {
+        case "rectangle": return shape.heigth * shape.width;
+        case "circle": return Math.PI * shape.radius ** 2;
+    }
+}
+
+const myRectangle: Rectangle = { kind: "rectangle", width: 10, heigth: 20 };
+console.log(`Rectangle's area is ${area(myRectangle)}`);
+
+const myCircle: Circle = { kind: "circle", radius: 10 };
+console.log(`Circle's area is ${area(myCircle)}`);
